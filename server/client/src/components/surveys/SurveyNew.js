8d393d124b5c1664d6f42223f28d5a4e762f672d
markhorvath <markhorvath7@gmail.com>
2020-05-27 18:47:36 -0400

    created SurveyFormReview, added helper func to SurveyNew to show FormReview component

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,13 +1,27 @@
 //SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview === true) {
+      return <SurveyFormReview />;
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
+    );
+  }
+
   render() {
     return (
       <div>
-      SurveyNew
-      <SurveyForm />
+      {this.renderContent()}
       </div>
     )
   }
